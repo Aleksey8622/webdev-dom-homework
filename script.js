cdbc9131c21a08ea131429fed8e5bec148ddbdeb
Алексей Руднев <aleksey.rudnev.ru@gmail.com>
2023-06-17 18:33:38 +0300

    changes file script.js

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 "use strict";
-const button = document.querySelectorAll('button');
-const list = document.querySelectorAll('ul');
-const inputName = document.querySelectorAll('.add-form-name');
-const inputComments = document.querySelectorAll('.add-form-text');
+const button = document.querySelector('.add-form-button');
+const list = document.querySelector('.comments');
+const inputName = document.querySelector('.add-form-name');
+const inputComments = document.querySelector('.add-form-text');
+
 const currentDate = (data) => {
 
-    const months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+  const months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
 
-    let day = data.getDate();
-    let month = months[data.getMonth()];
-    let year = data.getFullYear();
-    let hour = data.getHours() < 10 ? '0' + data.getHours() : data.getHours();
-    let minuts = data.getMinutes() < 10 ? '0' + data.getMinutes() : data.getMinutes();
-    let conclusion = `${day}.${month}.${year} ${hour}:${minuts}`;
-    return conclusion;
+  let day = data.getDate();
+  let month = months[data.getMonth()];
+  let year = data.getFullYear();
+  let hour = data.getHours() < 10 ? '0' + data.getHours() : data.getHours();
+  let minuts = data.getMinutes() < 10 ? '0' + data.getMinutes() : data.getMinutes();
+  let conclusion = `${day}.${month}.${year} ${hour}:${minuts}`;
+  return conclusion;
 }
 // const data = new Date();
 
 const disablingButton = () => {
-    if (inputName[0].value && inputComments[0].value) {
-        button[2].disabled = false;
+  if (inputName.value && inputComments.value) {
+    button.disabled = false;
 
-    } else {
-        button[2].disabled = true;
-    }
+  } else {
+    button.disabled = true;
+  }
 
 }
 disablingButton();
@@ -31,29 +32,29 @@ disablingButton();
 
 
 
-button[2].addEventListener('click', () => {
+button.addEventListener('click', () => {
 
-    inputName[0].classList.remove('error')
-    if (inputName[0].value === '') {
-        inputName[0].classList.add('error')
-    }
-    inputComments[0].classList.remove('error')
-    if (inputComments[0].value === '') {
-        inputComments[0].classList.add('error')
-        return;
-    }
+  inputName.classList.remove('error')
+  if (inputName.value === '') {
+    inputName.classList.add('error')
+  }
+  inputComments.classList.remove('error')
+  if (inputComments.value === '') {
+    inputComments.classList.add('error')
+    return;
+  }
 
-    const theWholeList = list[0].innerHTML;
+  const theWholeList = list.innerHTML;
 
-    list[0].innerHTML = theWholeList + `<ul class="comments">
+  list.innerHTML = theWholeList + `<ul class="comments">
     <li class="comment">
       <div class="comment-header">
-        <div>${inputName[0].value}</div>
+        <div>${inputName.value}</div>
         <div>${currentDate(new Date())}</div>
       </div>
       <div class="comment-body">
         <div class="comment-text">
-          ${inputComments[0].value}
+          ${inputComments.value}
         </div>
       </div>
       <div class="comment-footer">
@@ -64,10 +65,10 @@ button[2].addEventListener('click', () => {
       </div>
     </li>
   </ul>`
-    inputName[0].value = '';
-    inputComments[0].value = '';
-    button[2].disabled = true;
+  inputName.value = '';
+  inputComments.value = '';
+  button.disabled = true;
 })
 
-inputName[0].addEventListener('input', disablingButton);
-inputComments[0].addEventListener('input', disablingButton);
+inputName.addEventListener('input', disablingButton);
+inputComments.addEventListener('input', disablingButton);
